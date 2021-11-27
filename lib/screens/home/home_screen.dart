f7f5ccb55c2458c9d691c5a836feaf6f71f011db
SAUDAFAQ <saud.afaq123@gmail.com>
2021-11-27 18:21:27 +0100

    My First Commit

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,101 @@
+import 'package:dictionary_app/bloc/dictionary_cubit.dart';
+import 'package:dictionary_app/screens/list/list_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  getDictionaryFormWidget(BuildContext context) {
+    final cubit = context.watch<DictionaryCubit>();
+
+    return Container(
+      padding: const EdgeInsets.all(26),
+      child: Column(
+        children: [
+          Spacer(),
+          Text(
+            "Dictionary App",
+            style: TextStyle(
+              color: Colors.deepOrangeAccent,
+              fontSize: 34,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "Search any word you want quickly",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          TextField(
+            controller: cubit.queryControler,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              hintText: "Search a word",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(40),
+                borderSide: new BorderSide(color: Colors.deepOrangeAccent),
+              ),
+              fillColor: Colors.grey[100],
+              filled: true,
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.deepOrangeAccent,
+              ),
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+          Spacer(),
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                cubit.getWordSearched();
+              },
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.deepOrangeAccent,
+                  padding: const EdgeInsets.all(16)),
+              child: Text("SEARCH"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  getLoadingWidget() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+  getErrorWidget(message) {
+    return Center(child: Text(message));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.watch<DictionaryCubit>();
+
+    return BlocListener(
+      listener: (context, state) {
+        if (state is WordSearchedState && state.words != null) {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ListScreen(state.words)));
+        }
+      },
+      bloc: cubit,
+      child: Scaffold(
+          backgroundColor: Colors.blueGrey[900],
+          body: cubit.state is WordSearchingState
+              ? getLoadingWidget()
+              : cubit.state is ErrorState
+                  ? getErrorWidget("Some Error")
+                  : cubit.state is NoWordSearchedState
+                      ? getDictionaryFormWidget(context)
+                      : Container()),
+    );
+  }
+}
